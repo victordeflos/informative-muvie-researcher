6ca72896625bb17f09492bb4faa75fd3b25366b4
Viktor Blazhevski <victordeflos@hotmail.com>
2021-03-09 11:44:21 +0100

    fin

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => <div className="row">
+    <div className="col-md-4 col-xs-12"></div>
+    <div className="col-md-4 col-xs-12 text-center">
+        <img src="/images/2002.png" alt="Victor Blazhevski" className="img img-thumbnail"/>
+        <h3>Movie Informant App</h3>
+        <h6> REACT & Redux</h6>
+        <p>Powered from Node.js</p>s
+        
+    </div>
+    <div className="col-md-4 col-xs-12"></div>
+</div>;
+
+export default Home;
